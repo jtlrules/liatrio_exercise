cf9884bd480be0ed2e6f219e21f833235be6927b
jtlrules <joshuatylerlittle2@gmail.com>
2022-04-20 12:50:06 -0700

    Finished Express.js app

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+*/
+
+router.get('/', function(req, res, next) {
+	
+  const name = "Josh";
+  const timestamp = Date.now();
+  const ret_val =
+  {
+	  message: "My name is " + name,
+	  timestamp: timestamp
+  }
+  return res.json(ret_val);
+})
+
+module.exports = router;
